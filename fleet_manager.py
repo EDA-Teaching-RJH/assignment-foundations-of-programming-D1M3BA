369aa88d55ab8c3d7090d8df4ba727b182733eea
D1M3BA <oob7@kent.ac.uk>
2026-02-04 02:20:39 +0000

    Added display_menu
    prompts user for name
    prints options
    asks for use choice
    returns choice

--- fleet_manager.py
+++ fleet_manager.py
@@ -3,10 +3,10 @@
 def main():
     print("BOOTING SYSTEM...")
     print("...")
-    print("WELCOME TO FLEET COMMAND")
+    
 
     init_database()
-    
+    display_menu()
 
 
 
@@ -18,6 +18,28 @@ def init_database():
     id = ["1001" , "1002" , "1003" , "1004" , "1005"]
 
     return names, ranks, divisions, id
+
+
+def display_menu():
+
+    full_name = input("Enter your full name: ").strip().title()
+
+    print("\n--- MENU ---")
+
+    print(f"Logging in: {full_name} ")
+    print(f"Student: {full_name} is now logged in")
+    print("WELCOME TO FLEET COMMAND")
+    print("1. View Crew")
+    print("2. Add Crew")
+    print("3. Remove Crew")
+    print("4. Analyze Data")
+    print("5. Exit")
+    opt = input("Select option: ")
+    
+    return opt
+
+
+
 
 
 
